352b5fd1dca3ee329af33313534472656a28198b
Nick Schimek <cfme00@gmail.com>
2019-03-06 11:33:30 -0800

    Remove test for blank custom page
    This page is being used.

--- spec/features/site_customization/custom_pages_spec.rb
+++ spec/features/site_customization/custom_pages_spec.rb
@@ -87,24 +87,6 @@ feature "Custom Pages" do
         expect(page).to have_content("Print this info")
       end
 
-      scenario "Don't show subtitle if its blank" do
-        custom_page = create(:site_customization_page, :published,
-          slug: "slug-without-subtitle",
-          title_en: "Custom page",
-          subtitle_en: "",
-          content_en: "Text for new custom page",
-          print_content_flag: false
-        )
-
-        visit custom_page.url
-
-        expect(page).to have_title("Custom page")
-        expect(page).to have_selector("h1", text: "Custom page")
-        expect(page).to have_content("Text for new custom page")
-        expect(page).not_to have_selector("h2")
-        expect(page).not_to have_content("Print this info")
-      end
-
       scenario "Listed in more information page" do
         custom_page = create(:site_customization_page, :published,
           slug: "another-slug",
